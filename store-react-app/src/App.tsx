0de8376ca3dec8dfcee3c9303b9f1516bdc08e75
unknown <renecouture26@gmail.com>
2020-06-09 17:27:14 -0400

    added reactstrap and centered the content on the page

--- store-react-app/src/App.tsx
+++ store-react-app/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
+import {Container, Row, Col}from'reactstrap';
 import {SingleItemComponent,ReduxSingleItemComponent} from './components/SingleItemComponent'
-
+//import 'bootstrap/dist/css/bootstrap.min.css';//was not working for me this time
 
 // import {BrowserRouter as Router} from 'react-router-dom'
 // import {Route, Switch, useHistory, Redirect} from 'react-router';
@@ -11,9 +12,7 @@ import {SingleItemComponent,ReduxSingleItemComponent} from './components/SingleI
 //   loggedInUser : User | null;
 // }
 
-
 export class App extends React.Component<any, any>{
-
 
   // updateUser = (user:User | null) => {
   //   this.setState({
@@ -28,13 +27,22 @@ export class App extends React.Component<any, any>{
       
   //   }
   // }
-  
-  
 
   render(){
-    return(
-      <ReduxSingleItemComponent />
-    )
+    return(<>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+        <Container>
+          <Row>
+            <Col sm="1"></Col>
+            <Col sm="10">
+              <h4>Project 2</h4>
+              <p>We have some work to do!</p>
+              <ReduxSingleItemComponent />
+            </Col>
+            <Col sm="1"></Col>
+          </Row>
+        </Container>
+    </>)
   }
 }
 
